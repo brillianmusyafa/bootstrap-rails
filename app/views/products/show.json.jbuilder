9b9f4b71ef7fcecaf1c9ac03faf5f3d81092f35a
Brillian Musyafa <brillianmusyafa@gmail.com>
2016-04-01 18:09:33 +0700

    initial commit

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :name, :price, :notes, :created_at, :updated_at
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(name: 'Kapal Api', price: '1000')
+Product.create(name: 'Torabika Capucino', price: '1500')
+Product.create(name: 'ABC Mocca', price: '1000')
+Product.create(name: 'ABC Susu', price: '1000')
